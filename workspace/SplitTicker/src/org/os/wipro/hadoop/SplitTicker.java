1e4f0fd1feeb9eb96fa7042d2eba44c76e0d1807
Satyam <guptasatyam5878@gmail.com>
2017-06-09 00:27:22 +0530

    Split Ticker assignment completed

--- workspace/SplitTicker/src/org/os/wipro/hadoop/SplitTicker.java
+++ workspace/SplitTicker/src/org/os/wipro/hadoop/SplitTicker.java
@@ -3,12 +3,17 @@ package org.os.wipro.hadoop;
 import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
+
+
 
 public class SplitTicker {
 
@@ -24,7 +29,18 @@ public class SplitTicker {
 		job.setMapperClass(SplitTickerMapper.class);
 		job.setReducerClass(SplitTickerReducer.class);
 		
+		
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(Text.class);
+		
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+		
 		job.setNumReduceTasks(4);
+		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+
+		FileOutputFormat.setOutputPath(job,new Path(otherArgs[1]));
+
 		System.out.println(job.waitForCompletion(true));
 		
 	}
@@ -41,12 +57,15 @@ public class SplitTicker {
 			}
 			Text newValue = new Text(tmpString);
 			
-			context.write(newKey, newKey);	
+			context.write(newKey, newValue );	
 		}
 	}
 	public static class SplitTickerReducer extends Reducer<Text,Text,Text,Text>{
-			public void reduce(Text key, Iterable<Text> values,Context context){
+			public void reduce(Text key, Iterable<Text> values,Context context) throws IOException, InterruptedException{
 				//do nothing 
+				for(Text value: values){
+					context.write(key, value);
+				}
 			}
 	}
 	
